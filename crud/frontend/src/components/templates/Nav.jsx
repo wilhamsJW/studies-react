fd4ac98bf55034025c772f4fdc51d358b77f24d1
wilhamsJW <wilhamsmeira@gmail.com>
2020-08-19 18:25:08 -0300

    applying css to the nav

--- crud/frontend/src/components/templates/Nav.jsx
+++ crud/frontend/src/components/templates/Nav.jsx
@@ -3,5 +3,8 @@ import React from 'react'
 
 export default props => 
 <aside className="menu-area">
-    Navegation
+    <nav className="menu">
+        <a href="#/"><i className="fa fa-home"></i> Início&nbsp;</a>
+        <a href="#/users"><i className="fa fa-users"></i>&nbsp;Usuários</a>
+    </nav>
 </aside>
