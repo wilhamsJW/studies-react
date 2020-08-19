4a5399ee510679ba62df4a10217ad503934219d8
wilhamsJW <wilhamsmeira@gmail.com>
2020-08-19 17:21:50 -0300

    applying css to the Header

--- crud/frontend/src/main/App.jsx
+++ crud/frontend/src/main/App.jsx
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+
 import './App.css'
 import React from 'react'
 
@@ -10,6 +13,6 @@ export default props =>
     <div className="app">
         <Logo />
         <Nav />
-        <Main />
+        <Main icon="home" title="Início&nbsp;" subtitle="Projeto cadastro de usuários." /> {/*icon="home" = nome do ícone do awesome, coloca o nome do ícone aqui para passar como parametro pelo props.icon para o componente desejado  */}
         <Footer />
     </div>
