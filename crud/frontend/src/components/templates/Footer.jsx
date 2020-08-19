ffbbdd2f5ea8832bab18eb777514b64ed05cafee
wilhamsJW <wilhamsmeira@gmail.com>
2020-08-19 17:45:56 -0300

    applying css to the footer

--- crud/frontend/src/components/templates/Footer.jsx
+++ crud/frontend/src/components/templates/Footer.jsx
@@ -3,5 +3,5 @@ import React from 'react'
 
 export default props =>
     <footer className="footer"> 
-        Footer
+        <span>Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong>Wilhams<span className="text-danger"> Júnior</span></strong></span>
     </footer>
